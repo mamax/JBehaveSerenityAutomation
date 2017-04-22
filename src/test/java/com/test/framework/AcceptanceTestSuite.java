e240390f544fc677c1658630217052e927ca1133
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-04-23 00:09:02 +0300

    parallel_execution

--- src/test/java/com/test/framework/AcceptanceTestSuite.java
+++ src/test/java/com/test/framework/AcceptanceTestSuite.java
@@ -1,5 +1,3 @@
 package com.test.framework;
 
-import net.serenitybdd.jbehave.SerenityStories;
-
-public class AcceptanceTestSuite extends SerenityStories {}
+public class AcceptanceTestSuite extends ParallelAcceptanceTestSuite {}
